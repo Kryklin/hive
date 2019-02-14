ec908ea99f1ca25a0b9869eafafc9c16a78daa02
Kryklin <47632465+Kryklin@users.noreply.github.com>
2019-02-14 22:56:25 +0000

    Update SteamConfig.php

--- apply/steamauth/SteamConfig.php
+++ apply/steamauth/SteamConfig.php
@@ -1,7 +1,7 @@
 <?php
 //Version 3.2
-$steamauth['apikey'] = "ADA389E45E4959C0C3A9DC3F44413A11"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = "https://www.evo-network.net/"; // The main URL of your website displayed in the login page
+$steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = "index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "app.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
